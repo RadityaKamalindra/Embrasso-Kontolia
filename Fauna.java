bd94de4f3ed1482786710b73a2f3bccce59e9eca
Raditya Kamalindra <radityakamalindra1@gmail.com>
2025-10-23 09:35:59 +0700

    Update Fauna.java

--- Fauna.java
+++ Fauna.java
@@ -3,21 +3,34 @@ package Project;
 public class Fauna extends Makhluk {
     public String habitat;
     public String ciri;
+    public String Kingdom;
+ 
 
     public Fauna() {
         super.setType("Fauna");
         this.habitat = "Darat";
         this.ciri = "Berkaki Empat"; 
+        this.Kingdom = "Animalia";
+       
     }
 
-    public Fauna(String t, int u, String j, String n, double b, String h, String c) {
+    public Fauna(String t, int u, String j, String n, double b, String h, String c, String K) {
         super(t, u, j, n, b);
         this.habitat = h;
         this.ciri = c;
+        this.Kingdom = K;
     }
 
     public String getHabitat() {
-        return habitat;
+        return habitat; 
+    }
+
+    public String getKingdom() {
+        return Kingdom;
+    }
+
+    public void setKingdom(String Kingdom) {
+        this.Kingdom = Kingdom;
     }
 
     public String getCiri() {
@@ -36,5 +49,6 @@ public class Fauna extends Makhluk {
         super.print();
         System.out.println("Habitat: " + habitat);
         System.out.println("Ciri-ciri: " + ciri);
+        System.out.println("Kingdom: " + Kingdom);                      
     }
 }
