a47327a7b6005bbae347eea04861d8442ed781d4
Raditya Kamalindra <radityakamalindra1@gmail.com>
2025-10-23 09:34:25 +0700

    Refactor Brontosaurus class with new attributes
    Updated the Brontosaurus class to include a new 'berat' field and modified the constructor and print method to reflect changes.

--- Brontosaurus.java
+++ Brontosaurus.java
@@ -3,25 +3,34 @@ package Project;
 public class Brontosaurus extends Fauna {
     public String Periode;
     public String Ukuran;
+    public int berat; 
 
     public Brontosaurus() {
-        super.setUmur(70);
-        super.setJenis("Herbivora");
+
         super.setNama("Brontosaurus");
-        super.setBanyak(225000);
-        super.setHabitat("Amerika Serikat Bagian Barat");
-        super.setCiri("Leher dan ekornya yang sangat panjang, tubuhnya yang besar dan berkaki empat, kepala yang kecil dibandingkan dengan tubuhnya, serta tulang-tulangnya yang memiliki rongga udara");
+        super.setUmur(150);
+        super.setJenis("Herbivora");
+        super.setHabitat("Darat");
+        super.setCiri("Leher Panjang dan Berbadan Besar");
+        super.setKingdom("Animalia");
+        super.setBanyak(0);
         this.Periode = "Jura Akhir";
         this.Ukuran = "23 meter x 20 Ton";
+        this.berat = 20000;
     }
 
-    public Brontosaurus(String t, int u, String j, String n, double b, String h, String c) {
-        super(t, u, j, n, b, h, c);
-    } 
+    public Brontosaurus(String t, String c, int b) {
+        super();
+        this.Periode = t;
+        this.Ukuran = c;
+        this.berat = b;
+
+    }
 
     public void print() {
         super.print();
         System.out.println("Periode: " + Periode);
         System.out.println("Ukuran: " + Ukuran);
-    }
+        System.out.println("Berat: " + berat + " kg");
+    } 
 }
