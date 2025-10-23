f34db752eec806b110c1ae1510365cc734977797
Raditya Kamalindra <radityakamalindra1@gmail.com>
2025-10-23 09:36:32 +0700

    Update Flora.java

--- Flora.java
+++ Flora.java
@@ -1,21 +1,35 @@
 package Project;
 
 public class Flora extends Makhluk {
-    
+    public String Warna;
+    public String Bentuk;
+    public String Aroma;
+
+
     public Flora() {
-        super.setType("Flora");
-        super.setUmur(10);
-        super.setJenis("irritability");
-        super.setNama("Embrasso Kontolia");
-        super.setBanyak(2);
+        super.nama = "Embrasso Kontolia";
+        super.umur = 130;
+        super.jenis = "Irritability";
+        super.type = "Flora";
+        super.banyak = 1;
+        this.Warna = "Coklat";
+        this.Bentuk = "Panjang Melengkung, Berbatang Keras, Kepala Seperti Jamur";
+        this.Aroma = "Harum Pandan";
     }
     
-    public Flora(String t, int u, String j, String n, double b) {
+    public Flora(String t, int u, String j, String n, double b, String w, String B, String a) {
         super(t, u, j, n, b);
+        this.Warna = w;
+        this.Bentuk = B;
+        this.Aroma = a;
     }
 
     public void display() {
         super.print();
+        System.out.println("Warna: " + Warna);
+        System.out.println("Bentuk: " + Bentuk);
+        System.out.println("Aroma: " + Aroma);
+        
     }
    
 }
